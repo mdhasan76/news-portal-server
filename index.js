1d0cd456dd53312631c148a5cf4e841f81d17b35
Md Hasan <mdhasan8064@gmail.com>
2022-10-23 18:04:12 +0600

    add news file

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
     res.send('News API running');
+})
+
+app.get('/category/:cateId', (req, res) => {
+    const id = req.params.cateId;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === id);
+        res.send(selectedCategory)
+    }
+})
+
+app.get('/news/:newsId', (req, res) => {
+    const id = req.params.newsId;
+    const selectedItems = news.find(n => n._id === id);
+    res.send(selectedItems)
 })
 
 app.get('/news-categories', (req, res) => {
